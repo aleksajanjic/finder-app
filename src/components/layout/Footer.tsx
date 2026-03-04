de7f0d9890fb0ce356e9b42e8ea5a17966baa7ae
Aleksa Janjic <aleksajanjic@hotmail.com>
2026-03-04 18:57:29 +0100

    wip

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,11 @@
 function Footer() {
-  return <div className="footer-wrapper">this is a footer</div>;
+	return (
+		<div className="footer-wrapper">
+			<div>Finder • Find your next meal with friends</div>
+			<div>Sessions expire after 24 hours</div>
+			<div>Built with Supabase & React</div>
+		</div>
+	);
 }
 
 export default Footer;
